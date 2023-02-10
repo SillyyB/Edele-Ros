7f58c7b59fb89a8ea19a6a5816b6872bdc6a5cb9
Sil <silvanvb@ziggo.nl>
2023-02-10 15:11:31 +0100

    werkt eindelijk

--- manege-edele-ros/src/components/LoggedIn/Calendar.jsx
+++ manege-edele-ros/src/components/LoggedIn/Calendar.jsx
@@ -1,8 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
-import Lesson from "./Lesson";
-import { GiTeacher } from "react-icons/gi";
-import AvailableLessons from "./AvailableLessons";
-import CalendarNav from "./CalendarNav";
+import React, { useState } from "react";
 import CreateNewClass from "./Lessons/CreateNewClass";
 import LessonDescriptionBar from "./Lessons/LessonDescriptionBar";
 
@@ -11,10 +7,8 @@ const Calendar = () => {
 
   // state
   const [name, setName] = useState();
-  const [isDisabled, setIsDisabled] = useState("");
+  const [isDisabled, setIsDisabled] = useState({});
   const [myLessons, setMyLessons] = useState([]);
-  const toggleDisabledButton = () => setIsDisabled(!isDisabled);
-  const [open, setOpen] = useState(false);
 
   // state with array of objects
   const [availableLessons, setAvailableLessons] = useState([
@@ -23,46 +17,47 @@ const Calendar = () => {
       date: "06.02.2023",
       time: "12:00 - 13:30",
       teacher: "van Hop",
+      id: 1,
     },
     {
       name: "Springen",
       date: "08.03.2023",
       time: "10:00 - 11:30",
       teacher: "Rieter",
+      id: 2,
     },
   ]);
 
   // handle button click functions
   const handleClick = (index) => {
     // join class & show new list with your lessons
-    console.dir(availableLessons[index]);
     setMyLessons([...myLessons, availableLessons[index]]);
-    toggleDisabledButton(availableLessons[index]);
+    // prevent mulitple clicks
+    isDisabled[availableLessons[index].id] = true
+    setIsDisabled(isDisabled);
+    console.log(isDisabled)
   };
 
   const handleRemoveLesson = (index) => {
-    // delete lesson from your lessons
+    // delete lesson from 'your lessons'
     setMyLessons((oldValues) => {
       return oldValues.filter((_, i) => i !== index);
     });
+    // enable button
+    isDisabled[myLessons[index].id] = false
+    setIsDisabled(isDisabled);
+console.log(index)
+    console.log(isDisabled)
+  };
+
+  const handleCreateClass = () => {
+    setAvailableLessons([...availableLessons, { teacher: "Perenboom" }]);
   };
 
   return (
     <div className="">
       {/* <CalendarNav /> */}
 
-      {/* Create new class button*/}
-
-      {/* <div className="border-b border-gray-400 bg-gray-300 pt-3">
-        <button onClick={handleOpenCreateClass}>
-          <p>Les aanmaken</p>
-        </button>
-      </div>
-      {!openCreateClass ? null : (
-        <div className="bg-gray-200">
-          <p>title</p>
-        </div>
-      )} */}
       <div>
         <LessonDescriptionBar
           title="Doe mee aan de les!"
@@ -84,7 +79,13 @@ const Calendar = () => {
           </thead>
           {availableLessons.map((lesson, index) => (
             <tbody className="border-b">
-              <tr className="font-medium tracking-tight">
+              <tr
+                className={
+                  isDisabled[lesson.id]
+                    ? "font-medium tracking-tight"
+                    : "bg-gray-50 font-medium  tracking-tight"
+                }
+              >
                 <td className="flex flex-col py-1 px-4 pl-28" align="center">
                   <p className="pb-1 text-gray-500" align="start">
                     <span className="pr-2">Mo.</span> {lesson.date}
@@ -100,13 +101,19 @@ const Calendar = () => {
                   {lesson.name}
                 </td>
                 <td className="py-2 px-4 pl-[52px] text-gray-500">
-                  {" "}
                   <button
                     value={index}
                     onClick={(e) => handleClick(e.target.value)}
-                    className="rounded bg-gray-200 py-1 px-5 font-light text-emerald-800"
+                    disabled={isDisabled[lesson.id]}
+                    className={
+                      isDisabled[lesson.id]
+                        ? "cursor-not-allowed rounded  bg-gray-400 py-1 px-5 font-light text-gray-200"
+                        : "rounded bg-gray-200 py-1 px-5 font-light text-emerald-500 hover:bg-gray-300 hover:text-emerald-700"
+                    }
                   >
-                    Doe mee
+                    {isDisabled[lesson.id]
+                      ? "u bent ingeschreven!"
+                      : "doe mee aan deze les"}
                   </button>
                 </td>
               </tr>
@@ -118,45 +125,57 @@ const Calendar = () => {
       <LessonDescriptionBar
         icon="FiUserCheck"
         title="Uw lessen"
-        description="Schrijf u in voor de les door op de `Doe mee` knop te klikken!"
+        description="Hier ziet u alle lessen waar u voor bent ingeschreven"
       />
 
-      {/* <CreateNewClass /> */}
-
-      {/* title */}
-      <div className="">
-        <div>
-          {/* lessons map over  */}
-          <ul>
-            {myLessons.map((lesson, index) => (
-              <div key={index} className="border-b-2 border-black">
-                <h1 className="text-2xl uppercase tracking-wide">
-                  {lesson.name}
-                </h1>
-                <p className="text-lg tracking-wider text-gray-600">
-                  {lesson.date}
+      <table className="w-full table-auto font-sourceSansPro font-normal">
+        <thead className="">
+          <tr className="text-md border-b  uppercase  tracking-wide text-gray-600">
+            <th className="w-1/6 px-4 py-5 pl-28" align="start">
+              Datum & tijd
+            </th>
+            <th className="w-1/6 p-4 font-sourceSansPro" align="center">
+              Instructeur
+            </th>
+            <th className="p-4 pl-12" align="start">
+              Training
+            </th>
+          </tr>
+        </thead>
+        {myLessons.map((lesson, index) => (
+          <tbody className="border-b odd:bg-gray-50 even:bg-white">
+            <tr className="font-medium tracking-tight ">
+              <td className="flex flex-col py-1 px-4 pl-28" align="center">
+                <p className="pb-1 text-gray-500" align="start">
+                  <span className="pr-2">Mo.</span> {lesson.date}
                 </p>
+                <p className="text-gray-400" align="start">
+                  {lesson.time}
+                </p>
+              </td>
+              <td className="py-2 px-4 text-red-600" align="center">
+                {lesson.teacher}
+              </td>
+              <td className="py-2 px-4 pl-[52px] text-gray-500">
+                {lesson.name}
+              </td>
+              <td className="py-2 px-4  text-gray-500">
+                <button
+                  value={index}
+                  onClick={() => handleRemoveLesson(index)}
+                  className="rounded bg-red-700 px-5 py-1 tracking-wider text-white hover:bg-red-800"
+                >
+                  Schrijf mij uit
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
 
-                <p className="">3/6</p>
-                <div className="flex">
-                  <p>
-                    <GiTeacher className="text-3xl" />
-                  </p>
-                  <p className="text-xl text-zinc-500">{lesson.teacher}</p>
-                </div>
-                <div className="flex justify-end">
-                  <button
-                    value={index}
-                    onClick={() => handleRemoveLesson(index)}
-                    className="rounded bg-red-700 px-5 py-1 tracking-wider text-white"
-                  >
-                    Schrijf mij uit
-                  </button>
-                </div>
-              </div>
-            ))}
-          </ul>
-        </div>
+     {/* <CreateNewClass /> */}
+      <div className="pt-40">
+        <button onClick={handleCreateClass}>add</button>
       </div>
     </div>
   );
--- manege-edele-ros/src/components/LoggedIn/Profile.jsx
+++ manege-edele-ros/src/components/LoggedIn/Profile.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../logoutButton";
+import { BiUserCircle } from "react-icons/bi";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
   return (
     isAuthenticated && (
-      <article className="pt-20">
-        {user?.picture && <img src={user.picture} alt={user?.name}></img>}
-        <h2>{user?.name}</h2>
-        <ul>
+      <article className="mx-28 pt-20 font-sourceSansPro ">
+        <h2 className="mb-6 text-3xl  font-light text-gray-600">
+          Your profile
+        </h2>
+        <div className="flex justify-between rounded-3xl bg-gray-200 p-8">
+          <div className="flex">
+            {user?.picture && (
+              <img
+                className="h-35 w-36 rounded-full"
+                src={user.picture}
+                alt={user?.name}
+              ></img>
+            )}
+            <div className="pl-8">
+              <h2 className="pt-4 text-2xl tracking-wide text-gray-500 ">
+                {user?.name}
+              </h2>
+              <p className="pb-4 text-base font-light tracking-tight text-gray-500">
+                {user.email}
+              </p>
+              {/* <p className="font-light text-gray-500">
+                last name: {user.family_name}
+              </p> */}
+            </div>
+          </div>
+          <div className="flex items-start uppercase text-red-600">
+            <LogoutButton />
+          </div>
+        </div>
+        <ul className="space-y-1 font-light p-8">
           {Object.keys(user).map((objKey, i) => (
             <li key={i}>
               {objKey}: {user[objKey]}
